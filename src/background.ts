ebcdfeadec478a09741980e23ed7bd8fc7f0c363
michaeland974 <micanderson29@gmail.com>
2023-06-11 11:02:09 -0500

    feat: Foreground function added returning videos from selector

--- src/background.ts
+++ src/background.ts
@@ -2,7 +2,7 @@ console.log("From background");
 
 const injectScript = (tab: chrome.tabs.Tab) => {
   if(tab.url && tab.id && 
-    (tab.url).startsWith('https://www.youtube.com/')){
+     tab.url === 'https://www.youtube.com/'){
       chrome.scripting.executeScript({
         target: {tabId: tab.id},
         files: ['./foreground.ts']
--- src/foreground.ts
+++ src/foreground.ts
@@ -1,1 +1,29 @@
 console.log("From foreground");
+
+/*
+chips id rendering on multiples pages
+*/
+const chipBar: HTMLElement | null = document.getElementById('chips');
+const chips: HTMLCollection | undefined = chipBar?.children;
+
+const gridVideos: HTMLElement | null = document.getElementById('contents');
+
+const shortsSelector = '#contents > ytd-rich-section-renderer';
+const shorts: HTMLElement | null = document.querySelector(shortsSelector);
+
+const getVideoElements = (parent: HTMLElement | null, 
+                   selector: string): Element[] | [] => {
+  const videosContainer = parent?.querySelector(selector);
+  const videos = videosContainer?.children;
+  
+  return (videos===undefined) ? [] : [...videos];                  
+};
+
+
+
+
+
+
+
+
+
