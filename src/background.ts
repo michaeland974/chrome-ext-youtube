56268d11cd78ee437d18f31033bc22f43fbd8c06
michaeland974 <micanderson29@gmail.com>
2023-06-13 11:05:01 -0500

    fix: Tab listener fixed so script injection will happen on new tabs

--- src/background.ts
+++ src/background.ts
@@ -1,46 +1,29 @@
-type Tab = {
-  status: boolean,
-  browserTab: chrome.tabs.Tab
-}
-
-(async () => {
+(() => {
   const targetUrl = 'https://www.youtube.com/';
-  const {status, browserTab}: Tab = await setTabListenerOnUpdate(targetUrl);
-  if(status === true){
-    injectScript(browserTab, targetUrl);
-  }
+  listener(targetUrl, injectScript);  
 })();
 
-function injectScript(tab: chrome.tabs.Tab, targetUrl: string) {
-  if(tab.url && tab.id && 
-     tab.url === targetUrl){
-       chrome.scripting.executeScript({
-         target: {tabId: tab.id},
-         files: ['./foreground.js']
-       });
+type TabInject = {
+  tab: chrome.tabs.Tab, 
+  targetUrl: string
+}
+
+function listener(targetUrl: string, callback: (tabInject: TabInject) => void){
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if(tab.url === targetUrl && tab.status === 'complete'){
+      callback({tab, targetUrl});
+    }
+  })
+}
+
+function injectScript(tabInject: TabInject) {
+  const {tab, targetUrl} = tabInject;
+
+  if(tab.url && tab.id && tab.url === targetUrl){
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      files: ['./foreground.js']
+    });
   }
 }
 
-function setTabListenerOnUpdate(targetUrl: string): Promise<Tab>{
-  let statusFlag = false;
-    
-  return new Promise((resolve) => {
-    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-      if(statusFlag){
-        resolve({status: statusFlag, browserTab: tab});
-      }
-      //Check tab status to prevent multiple fires
-      if(tab.url === targetUrl && tab.status === 'complete'){
-        statusFlag = true;
-      }
-    })
-  });
-}
-
-// function setTabListenerOnUpdate(targetUrl: string): void{
-//   chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//       if(tab.url === targetUrl && tab.status === 'complete'){
-//         injectScript(tab, targetUrl);
-//       }
-//   })
-// }
--- src/foreground.ts
+++ src/foreground.ts
@@ -1,4 +1,4 @@
-import { YoutubeElement, DOMManipulator } from "./classes/DOMManipulator"
+import { YoutubeElement, DOMManipulator } from './classes/DOMManipulator'
 
 const Shorts: YoutubeElement = {
   selector: '#contents #content #contents',
@@ -14,12 +14,6 @@ const chipBar: HTMLElement | null = document.getElementById('chips');
 const chips: HTMLCollection | undefined = chipBar?.children;
 
 const YouTubeDOM = new DOMManipulator(Shorts, Grid);
-// console.log(YouTubeDOM.videos().grid);
-
-const config = { childList: true };
-const observer = new MutationObserver(() => console.log(YouTubeDOM.videos().grid));
-  observer.observe(YouTubeDOM.parentElements.grid!, config);
-
 
 
                      
