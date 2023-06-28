eb27862e239cfd5ef6c0acbfd86faf96d25e8a09
michaeland974 <micanderson29@gmail.com>
2023-06-27 20:12:27 -0500

    cleanup

--- src/background.ts
+++ src/background.ts
@@ -32,16 +32,11 @@ function injectScript (tabInject: TabInject) {
 
 function injectCSS (tabInject: TabInject) {
   const {tab, targetUrl} = tabInject;
-    if(tab.id && tab.url && tab.url !== targetUrl){
-        chrome.scripting.insertCSS({
-          target: { tabId: tab.id },
-          files: ['./styles/reset.css']
-        });
-    } else if(tab.id){
-        chrome.scripting.insertCSS({
-          target: { tabId: tab.id },
-          files: ['./styles/homepage.css', './styles/guide.css']
-        });
+    if(tab.id){
+      chrome.scripting.insertCSS({
+        target: { tabId: tab.id },
+        files: ['./styles/homepage.css', './styles/guide.css']
+      });
     }
 }
 
